8444e559fa596b356ce2e9cd32451001f8e704a2
dmmuralitharan <dmmuralitharan@gmail.com>
2025-06-06 23:14:18 +0530

    add: useForm, register, handleSubmit in login form

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,16 +1,34 @@
+import { useForm } from "react-hook-form"
 
 export default function LoginForm() {
+
+    const { register, handleSubmit } = useForm()
+
+    const onSubmitLoginForm = (data) => {
+        console.log(data);
+    }
+
     return (
-        <form className="flex flex-col space-y-2 mt-10">
+        <form 
+            onSubmit={handleSubmit(onSubmitLoginForm)}
+            className="flex flex-col space-y-2 mt-10">
 
             <div className="flex space-x-5 justify-center items-center">
                 <label htmlFor="username">Username</label>
-                <input type="text" id='username' className="bg-slate-50 text-slate-800 font-semibold px-3 py-1 rounded-sm outline-none border-none" />
+                <input 
+                    type="text" 
+                    id='username'
+                    {...register("username")} 
+                    className="bg-slate-50 text-slate-800 font-semibold px-3 py-1 rounded-sm outline-none border-none" />
             </div>
 
             <div className="flex space-x-6.5 justify-center items-center">
                 <label htmlFor="password">Password</label>
-                <input type="password" id='password' className="bg-slate-50 text-slate-800 font-semibold px-3 py-1 rounded-sm outline-none border-none " />
+                <input 
+                    type="password" 
+                    id='password' 
+                    {...register("password")}
+                    className="bg-slate-50 text-slate-800 font-semibold px-3 py-1 rounded-sm outline-none border-none " />
             </div>
 
             <div className="flex justify-center items-center">
